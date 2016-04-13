5050c566932e1cffed408a3552edc987497c4356
thesuperkareem <thesuperkareem@users.noreply.github.com>
2016-04-13 21:26:07 +0200

    Book Edited.java
    hgh

---
+++ book.java
@@ -0,0 +1,42 @@
+
+ public class Book {
+     private String name ;
+     private String serial ;
+     private float price ;
+     private int numberOfCopies;
+     
+     public Book(String name, String serial , float price , int numberOfCopies){
+     this.name=name;
+     this.serial=serial;
+     this.price=price;
+     this.numberOfCopies=numberOfCopies;
+     
+     } //Consturctor
+ 
+     
+     public void setName (String x){
+         name = x;
+     }
+     public String getName (){
+         return name;
+     }
+     public void setSerial (String x){
+         serial = x;
+     }
+     public String getSerial (){
+         return serial;
+     }
+     public void setPrice (float x){
+         price = x;
+     }
+     public float getPrice (){
+         return price;
+     }
+     public void setNumberOfCopies (int y){
+         numberOfCopies = y;
+     }
+     public int getNumberOfCopies (){
+         return numberOfCopies;
+     }
+   
+ }
